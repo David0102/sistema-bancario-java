d86165ed97e227c5e9e52a9d4fa169b9689b81be
David0102 <davidmoura0102@gmail.com>
2023-10-03 12:48:00 -0300

    create sistema_bancario.jar

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import cliente.Cliente;
+import conta.Conta;
+
 public class Main {
     public static void main(String[] args) {
+        Cliente cliente = new Cliente("David", "Moura", "000.000.000-00");
+        Conta conta = new Conta(cliente, 01, "Caixa", "03/10/2023");
+
+        System.out.println(conta.recuperaDadosParaImpressao());
     }
 }
